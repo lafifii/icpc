d2daa982fb2a7709e0abb6529fdb826a2f6ba9a8
oscarburga <51001981+oscarburga@users.noreply.github.com>
2020-09-19 14:57:56 -0500

    Update point-basic-operations.cpp

--- Geometry/point-basic-operations.cpp
+++ Geometry/point-basic-operations.cpp
@@ -5,7 +5,7 @@ template<typename T> struct pt{
 	pt operator*(const T k){ return { x * k, y * k }; }
 	T operator%(const pt& q) { return (x * q.y) - (y * q.x); }
 	T len2() { return x*x + y*y; }
-	T len() { return sqrt(len2()); }
+	double len() { return sqrt(len2()); }
 	bool operator<(const pt&q) { return (x < q.x) || (x == q.x && y < q.y); }
 	bool operator==(const pt&q) { return x == q.x && y == q.y; }
 };
